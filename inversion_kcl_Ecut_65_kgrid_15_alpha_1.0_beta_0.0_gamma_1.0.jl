f10861b90fb779f9a7991ca4ea0870010ab27fe2
vebjorhb <vebjorn.bakkestuen@oslomet.no>
2026-05-08 14:59:38 +0200

    inversion KCl

---
+++ inversion_kcl_Ecut_65_kgrid_15_alpha_1.0_beta_0.0_gamma_1.0.jl
@@ -0,0 +1,7 @@
+include("inversion.jl")
+reference = "results/reference_kcl_Ecut_65_kgrid_15.jld2"
+prefix, _ = splitext(@__FILE__)
+prefix = joinpath("results", prefix)
+εs = exp10.(0:-0.25:-7)
+run_exact_inversion(prefix, reference; verbose=true, retry=2,
+                    δ=1e-2, method=InversionVxcMod(1.0, 0.0, 1.0), εs, ρtol=5e-13)
